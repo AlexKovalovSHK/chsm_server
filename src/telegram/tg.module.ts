1ef3b501256582c4f427a423b23a23f145e09697
AlexKovalovSHK <alex@shk-info.de>
2026-05-01 12:53:26 +0200

    forwardRef

--- src/telegram/tg.module.ts
+++ src/telegram/tg.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TgInternalService } from './service/tg-internal.service';
 import { TgInternalController } from './controller/tg-internal.controller';
 import { BotApiService } from './service/bot-api.service';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [forwardRef(() => UsersModule)],
   providers: [TgInternalService, BotApiService],
   exports: [TgInternalService, BotApiService],
   controllers: [TgInternalController],
