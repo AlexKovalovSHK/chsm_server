edb090557f00da3e110fd11923226ea1fb398305
AlexKovalovSHK <alex@shk-info.de>
2026-05-13 21:25:00 +0200

    add auth tma

--- src/main.ts
+++ src/main.ts
@@ -67,6 +67,7 @@ async function bootstrap() {
       const allowedOrigins = [
         'https://chsm.shk.solutions',
         'https://chsm.pro',
+        'https://tma.chsm.pro',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://localhost:3000',
